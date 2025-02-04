222abab79d3a08367d02c63a347d59ffd6dbd9f8
Emil Navntoft Pedersen <emilnavntoft@gmail.com>
2025-02-04 19:58:37 +0100

    Add project files.

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+﻿function saveAsFile(filename, bytesBase64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    document.body.appendChild(link); // Needed for Firefox
+    link.click();
+    document.body.removeChild(link);
+}
+
+function updatePH() {
+    const T = Module.PropsSI("T", "P", 100000, "T", 300, "R134a");
+    const rho = Module.PropsSI("D", "P", 100000, "T", 300, "R134a");
+    const p = Module.PropsSI("P", "P", 100000, "T", 300, "R134a");
+    const s = Module.PropsSI("S", "P", 100000, "T", 300, "R134a");
+    const h = Module.PropsSI("H", "P", 100000, "T", 300, "R134a");
+    const cp = Module.PropsSI("C", "P", 100000, "T", 300, "R134a");
+
+    console.log(h);
+}
