50f94964aed2d395be67352e8fa25a68882f8d70
Emil Navntoft Pedersen <emilnavntoft@gmail.com>
2025-02-05 20:09:25 +0100

    c

--- obj/Debug/net8.0/EmilNavntoft.github.io.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmilNavntoft.github.io.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmilNavntoft.github.io")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+222abab79d3a08367d02c63a347d59ffd6dbd9f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c020dbfa07d52e39c50ec82accef3884669e58")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmilNavntoft.github.io")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmilNavntoft.github.io")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
