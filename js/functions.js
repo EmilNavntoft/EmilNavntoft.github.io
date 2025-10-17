b0ac4c29143402a5d911ca2b783c02b24a4a16c5
emil <emilnavntoft@gmail.com>
2025-10-17 20:29:41 +0200

    ny ver

--- js/functions.js
+++ js/functions.js
@@ -9,14 +9,17 @@ export function PropsSI(prop0, prop1, val1, prop2, val2, fluid) {
     console.log(fluid);
     const value = Module.PropsSI(prop0, prop1, val1, prop2, val2, fluid);
     console.log(value);
-    return value;
+    return value;   
 }
 
-function saveAsFile(filename, bytesBase64) {
-    var link = document.createElement('a');
-    link.download = filename;
-    link.href = "data:application/octet-stream;base64," + bytesBase64;
-    document.body.appendChild(link); // Needed for Firefox
-    link.click();
-    document.body.removeChild(link);
+window.saveAsFile = async function (fileName, contentStreamReference) {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
 }
