3d9668ac23cb4a4b77df095ccc18efb7a501501f
emil <emilnavntoft@gmail.com>
2025-10-18 17:19:45 +0200

    ver

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,21 @@
-﻿import Module from "./coolprop.js"
+﻿let coolpropInstance = null;
+
+window.initializeCoolprop = async function () {
+    if (!coolpropInstance) {
+        const Module = (await import("./coolprop.js")).default;
+        coolpropInstance = await Module();
+    }
+}
 
 export function PropsSI(prop0, prop1, val1, prop2, val2, fluid) {
-    console.log(prop0);
-    console.log(prop1);
-    console.log(prop2);
-    console.log(val1);
-    console.log(val2);
-    console.log(fluid);
-    const value = Module.PropsSI(prop0, prop1, val1, prop2, val2, fluid);
+    if (!coolpropInstance) {
+        throw new Error("CoolProp not initialized. Call initialize() first.");
+    }
+
+    console.log(prop0, prop1, val1, prop2, val2, fluid);
+    const value = coolpropInstance.PropsSI(prop0, prop1, val1, prop2, val2, fluid);
     console.log(value);
-    return value;   
+    return value;
 }
 
 window.saveAsFile = async function (fileName, contentStreamReference) {
